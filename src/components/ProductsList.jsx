58128914bddd52b4f5663244e6732a2aebf98c6a
Mahmood Moqadasi <mahmood1380.mm@gmail.com>
2024-10-17 20:37:50 +0330

    product list created

--- src/components/ProductsList.jsx
+++ src/components/ProductsList.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import styles from "./ProductsList.module.css";
 import { AiOutlineControl } from "react-icons/ai";
+import { BsTrash } from "react-icons/bs";
+import { FaRegPenToSquare } from "react-icons/fa6";
 function ProductsList() {
   return (
     <div className={styles.container}>
@@ -11,26 +13,32 @@ function ProductsList() {
           <AiOutlineControl size={30} color="rgba(41, 45, 50, 1)" />
         </div>
       </div>
-      {/* <div className={styles.table}></div> */}
-      <table className={styles.table}>
-        <tr>
-          <th></th>
-          <th>شناسه کالا</th>
-          <th>قیمت</th>
-          <th>موجودی</th>
-          <th>نام کالا</th>
-        </tr>
-        <tr>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td>
-            <button></button>
-            <button></button>
-          </td>
-        </tr>
-      </table>
+
+      <div className={styles.tableContainer}>
+        <table className={styles.table}>
+          <tr>
+            <th></th>
+            <th>شناسه کالا</th>
+            <th>قیمت</th>
+            <th>موجودی</th>
+            <th>نام کالا</th>
+          </tr>
+          <tr>
+            <td>
+              <button>
+                <BsTrash size={20} color="rgba(244, 63, 94, 1)" />
+              </button>
+              <button>
+                <FaRegPenToSquare size={20} color="rgba(74, 222, 128, 1)" />
+              </button>
+            </td>
+            <td>asdfadfadf</td>
+            <td>90 هزار</td>
+            <td>239</td>
+            <td>تیشرت</td>
+          </tr>
+        </table>
+      </div>
     </div>
   );
 }
