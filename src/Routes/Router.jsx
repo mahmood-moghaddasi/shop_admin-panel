0b18dc9a396476165bf6becbac9f4b62a9845d9f
Mahmood Moqadasi <mahmood1380.mm@gmail.com>
2024-10-28 10:32:14 +0330

    login navigation handled

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -2,16 +2,20 @@ import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import AdminPanelPage from "../pages/AdminPanelPage";
 import RegistrationPage from "../pages/RegistrationPage";
 import LoginPage from "../pages/LoginPage";
+import AuthProvider from "../providers/AuthProvider";
 import { getCookie } from "../utils/cookie";
 function Router() {
   const token = getCookie("token");
-  console.log(token);
   return (
     <BrowserRouter>
       <Routes>
         <Route
           path="/"
-          element={token ? <AdminPanelPage /> : <Navigate to="/login" />}
+          element={
+            <AuthProvider>
+              <AdminPanelPage />
+            </AuthProvider>
+          }
         />
         <Route
           path="/register"
